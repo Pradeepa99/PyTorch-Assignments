f22278b46533539fd53b0a11814f2f8e29f24012
Pradeepa <pradeepa@multicorewareinc.com>
2021-08-23 14:04:43 +0530

    FIX:Loop usage

--- source/linspace-app.cpp
+++ source/linspace-app.cpp
@@ -59,69 +59,118 @@ torch::Tensor compute_linspace(T start, T end, T steps)
 int main()
 {
 
-  torch::Tensor int_result1, int_result2, float_result1, float_result2;
-
-  /* Int data type */
-  int start[] = {3, 2, -10, 25, -5, -10, 6, 52};
-  int end[] = {10, 5, 10, 100, 10, 10, 78, 60};
-
-  /* Float data type*/
-  float f_start[] = {-2.5, 3.4, 23.3, 20.6, 11.1, -10.2};
-  float f_end[] = {10.2, 10.5, 40.2, 30.2, 57.3, -5.2};
-
-  int steps[] = {5, 4, 5, 6, 6, 5, 10, 0};
-
-  int float_count = 0, int_count = 0, total_count;
+  torch::Tensor float_result1, float_result2, float_result3, float_result4, float_result5, float_result6;
+  torch::Tensor int_result1, int_result2, int_result3, int_result4, int_result5, int_result6, int_result7, int_result8;
+  torch::Tensor float_compute_result1, float_compute_result2, float_compute_result3, float_compute_result4, float_compute_result5, float_compute_result6;
+  torch::Tensor int_compute_result1, int_compute_result2, int_compute_result3, int_compute_result4, int_compute_result5, int_compute_result6, int_compute_result7, int_compute_result8;
 
   try
   {
 
-    for (int i = 0; i < 6; i++)
-    {
+    /* Built in linspace - Float data type */
 
-      float_count++;
+    float_result1 = torch::linspace(-2.5, 10.2, 5);  //start is negative
+    /* Both start and end is positive */
+    float_result2 = torch::linspace(3.4, 10.5, 4);  
+    float_result3 = torch::linspace(23.3, 40.2, 5);
+    float_result4 = torch::linspace(20.6, 30.2, 6);
+    float_result5 = torch::linspace(11.1, 57.3, 6); 
+    float_result6 = torch::linspace(-10.2, -5.2, 5); //Both start and end is negative
 
-      if (steps[i] == 0)
-      {
+    /* Custom op for linspace - Float data type */
 
-        throw MyException();
-      }
+    float_compute_result1 = compute_linspace<float>(-2.5, 10.2, 5);
+    float_compute_result2 = compute_linspace<float>(3.4, 10.5, 4);
+    float_compute_result3 = compute_linspace<float>(23.3, 40.2, 5);
+    float_compute_result4 = compute_linspace<float>(20.6, 30.2, 6);
+    float_compute_result5 = compute_linspace<float>(11.1, 57.3, 6);
+    float_compute_result6 = compute_linspace<float>(-10.2, -5.2, 5);
 
-      float_result1 = torch::linspace(f_start[i], f_end[i], steps[i]);         //Built in linspace
-      float_result2 = compute_linspace<float>(f_start[i], f_end[i], steps[i]); //Custom op for linspace
+    /* Built in linspace - Int data type */
 
-      /* Printing the values to check the output*/
-      std::cout << "Float Linspace output" << std::endl;
-      std::cout << float_result1 << std::endl;
-      std::cout << "Float Custom ops output" << std::endl;
-      std::cout << float_result2 << std::endl;
+    int_result1 = torch::linspace(3, 10, 5); //Both start and end is positive
+    int_result2 = torch::linspace(2, 5, 4);
+    int_result3 = torch::linspace(-100, 10, 5); //start is negative
+    int_result4 = torch::linspace(25, 100, 6);
+    int_result5 = torch::linspace(-5, 10, 6);
+    int_result6 = torch::linspace(-10, 10, 5);
+    int_result7 = torch::linspace(6, 78, 10);
+    int_result8 = torch::linspace(52, 60, 0); //step is zero
 
-      assert(torch::equal(float_result1, float_result2));
-    }
+    /* Custom op for linspace - Int data type */
 
-    for (int i = 0; i < 8; i++)
-    {
+    int_compute_result1 = compute_linspace<int>(3, 10, 5);
+    int_compute_result2 = compute_linspace<int>(2, 5, 4);
+    int_compute_result3 = compute_linspace<int>(-100, 10, 5);  
+    int_compute_result4 = compute_linspace<int>(25, 100, 6);
+    int_compute_result5 = compute_linspace<int>(-5, 10, 6);
+    int_compute_result6 = compute_linspace<int>(-10, 10, 5);
+    int_compute_result7 = compute_linspace<int>(6, 78, 10);
+    int_compute_result8 = compute_linspace<int>(52, 60, 0);  
 
-      int_count++;
+    /* Printing the values to check the Builtin output for Float Test cases */
 
-      if (steps[i] == 0)
-      {
+    std::cout << "Float Linspace output" << std::endl;
+    std::cout << float_result1 << std::endl;
+    std::cout << float_result2 << std::endl;
+    std::cout << float_result3 << std::endl;
+    std::cout << float_result4 << std::endl;
+    std::cout << float_result5 << std::endl;
+    std::cout << float_result6 << std::endl;
 
-        throw MyException();
-      }
+    /* Printing the values to check the Custom ops output for Float Test cases */
 
-      int_result1 = torch::linspace(start[i], end[i], steps[i]);       //Built in linspace
-      int_result2 = compute_linspace<int>(start[i], end[i], steps[i]); //Custom op for linspace
+    std::cout << "Float Custom ops output" << std::endl;
+    std::cout << float_compute_result1 << std::endl;
+    std::cout << float_compute_result2 << std::endl;
+    std::cout << float_compute_result3 << std::endl;
+    std::cout << float_compute_result4 << std::endl;
+    std::cout << float_compute_result5 << std::endl;
+    std::cout << float_compute_result6 << std::endl;
 
-      /* Printing the values to check the output*/
+    /* Assert to check the results for Float Test cases */
 
-      std::cout << "Int Linspace output" << std::endl;
-      std::cout << int_result1 << std::endl;
-      std::cout << "Custom ops output" << std::endl;
-      std::cout << int_result2 << std::endl;
+    assert(torch::equal(float_result1, float_compute_result1));
+    assert(torch::equal(float_result2, float_compute_result2));
+    assert(torch::equal(float_result3, float_compute_result3));
+    assert(torch::equal(float_result4, float_compute_result4));
+    assert(torch::equal(float_result5, float_compute_result5));
+    assert(torch::equal(float_result6, float_compute_result6));
 
-      assert(torch::equal(int_result1, int_result2));
-    }
+    /* Printing the values to check the Built in output for Int Test cases*/
+
+    std::cout << "Int Linspace output" << std::endl;
+    std::cout << int_result1 << std::endl;
+    std::cout << int_result2 << std::endl;
+    std::cout << int_result3 << std::endl;
+    std::cout << int_result4 << std::endl;
+    std::cout << int_result5 << std::endl;
+    std::cout << int_result6 << std::endl;
+    std::cout << int_result7 << std::endl;
+    std::cout << int_result8 << std::endl;
+
+    /* Printing the values to check the Custom ops output for Int Test cases*/
+
+    std::cout << "Int Custom ops output" << std::endl;
+    std::cout << int_compute_result1 << std::endl;
+    std::cout << int_compute_result2 << std::endl;
+    std::cout << int_compute_result3 << std::endl;
+    std::cout << int_compute_result4 << std::endl;
+    std::cout << int_compute_result5 << std::endl;
+    std::cout << int_compute_result6 << std::endl;
+    std::cout << int_compute_result7 << std::endl;
+    std::cout << int_compute_result8 << std::endl;
+
+    /* Assert to check the results for Int Test cases */
+
+    assert(torch::equal(int_result1, int_compute_result1));
+    assert(torch::equal(int_result2, int_compute_result2));
+    assert(torch::equal(int_result3, int_compute_result3));
+    assert(torch::equal(int_result4, int_compute_result4));
+    assert(torch::equal(int_result5, int_compute_result5));
+    assert(torch::equal(int_result6, int_compute_result6));
+    assert(torch::equal(int_result7, int_compute_result7));
+    assert(torch::equal(int_result8, int_compute_result8));
   }
 
   catch (MyException &e)
@@ -129,8 +178,5 @@ int main()
     std::cout << e.what_exception() << std::endl;
   }
 
-  total_count = float_count + int_count;
-
-  std::cout << "Total test cases passed:" << total_count << std::endl
-            << std::endl;
+ 
 }
